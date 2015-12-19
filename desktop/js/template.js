9622061dab36f259d00e1c73fde4c3bf4c8b079f
Loïc <loic@jeedom.com>
2015-12-19 18:51:19 +0100

    Improvement

--- desktop/js/template.js
+++ desktop/js/template.js
@@ -29,7 +29,7 @@ function addCmdToTable(_cmd) {
     }
     var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">';
     tr += '<td>';
-    tr += '<span class="cmdAttr" data-l1key="id"></span>';
+    tr += '<span class="cmdAttr" data-l1key="id" style="display:none;"></span>';
     tr += '<input class="cmdAttr form-control input-sm" data-l1key="name" style="width : 140px;" placeholder="{{Nom}}"></td>';
     tr += '</td>';
     tr += '<td>';
