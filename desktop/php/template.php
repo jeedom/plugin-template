7cfe51b9f7af22721421665eca0e5e1c2d6e1959
Loïc <loic@jeedom.com>
2016-08-13 16:48:29 +0200

    Update template.php

--- desktop/php/template.php
+++ desktop/php/template.php
@@ -88,7 +88,7 @@ foreach (object::all() as $object) {
                </div>
            </div>
 	<div class="form-group">
-		<label class="col-sm-2 control-label"></label>
+		<label class="col-sm-3 control-label"></label>
 		<div class="col-sm-9">
 			<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
 			<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
