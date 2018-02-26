e4a98021500f55e6f300fceb8e4debaf5f3e81f1
ColonelMoutarde <4697568+ColonelMoutarde@users.noreply.github.com>
2018-02-26 14:26:27 +0100

    Exception are in global namespace

--- core/ajax/template.ajax.php
+++ core/ajax/template.ajax.php
@@ -21,16 +21,14 @@ try {
     include_file('core', 'authentification', 'php');
 
     if (!isConnect('admin')) {
-        throw new Exception(__('401 - Accès non autorisé', __FILE__));
+        throw new \Exception(__('401 - Accès non autorisé', __FILE__));
     }
     
     ajax::init();
 
-
-
-    throw new Exception(__('Aucune méthode correspondante à : ', __FILE__) . init('action'));
+    throw new \Exception(__('Aucune méthode correspondante à : ', __FILE__) . init('action'));
     /*     * *********Catch exeption*************** */
-} catch (Exception $e) {
+} catch (\Exception $e) {
     ajax::error(displayExeption($e), $e->getCode());
 }
 
