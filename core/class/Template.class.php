0615dd9adaab3721917dd074a3e2a000febefc4f
ColonelMoutarde <4697568+ColonelMoutarde@users.noreply.github.com>
2018-03-13 15:27:39 +0100

    Séparation en 2 classes pour une meilleure maintenabilité

---
+++ core/class/Template.class.php
@@ -0,0 +1,116 @@
+<?php
+
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* * ***************************Includes********************************* */
+require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+
+class Template extends eqLogic
+{
+    /*     * *************************Attributs****************************** */
+
+
+
+    /*     * ***********************Methode static*************************** */
+
+    /*
+     * Fonction exécutée automatiquement toutes les minutes par Jeedom
+      public static function cron() {
+
+      }
+     */
+
+
+    /*
+     * Fonction exécutée automatiquement toutes les heures par Jeedom
+      public static function cronHourly() {
+
+      }
+     */
+
+    /*
+     * Fonction exécutée automatiquement tous les jours par Jeedom
+      public static function cronDaily() {
+
+      }
+     */
+
+
+
+    /*     * *********************Méthodes d'instance************************* */
+
+    public function preInsert()
+    {
+        
+    }
+
+    public function postInsert()
+    {
+        
+    }
+
+    public function preSave()
+    {
+        
+    }
+
+    public function postSave()
+    {
+        
+    }
+
+    public function preUpdate()
+    {
+        
+    }
+
+    public function postUpdate()
+    {
+        
+    }
+
+    public function preRemove()
+    {
+        
+    }
+
+    public function postRemove()
+    {
+        
+    }
+
+    /*
+     * Non obligatoire mais permet de modifier l'affichage du widget si vous en avez besoin
+      public function toHtml($_version = 'dashboard') {
+
+      }
+     */
+
+    /*
+     * Non obligatoire mais ca permet de déclencher une action après modification de variable de configuration
+      public static function postConfig_<Variable>() {
+      }
+     */
+
+    /*
+     * Non obligatoire mais ca permet de déclencher une action avant modification de variable de configuration
+      public static function preConfig_<Variable>() {
+      }
+     */
+
+    /*     * **********************Getteur Setteur*************************** */
+}
---
+++ core/class/TemplateCmd.class.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* * ***************************Includes********************************* */
+require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+
+class TemplateCmd extends cmd
+{
+    /*     * *************************Attributs****************************** */
+    /*     * ***********************Methode static*************************** */
+    /*     * *********************Methode d'instance************************* */
+    /*
+     * Non obligatoire permet de demander de ne pas supprimer les commandes même si elles ne sont pas dans la nouvelle configuration de l'équipement envoyé en JS
+      public function dontRemoveCmd() {
+      return true;
+      }
+     */
+
+    public function execute($_options = array())
+    {
+        
+    }
+
+    /*     * **********************Getteur Setteur*************************** */
+}
