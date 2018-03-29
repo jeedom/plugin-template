385741d2d81c3d8603bbee9ab260c5c4f3825a73
Sylvain Dangin <sylvain.dangin@gmail.com>
2018-03-29 20:27:17 +0200

    Diversité des exemples Ajout des traductions

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -1,4 +1,5 @@
 <?php
+
 /* This file is part of Jeedom.
  *
  * Jeedom is free software: you can redistribute it and/or modify
@@ -16,32 +17,35 @@
  */
 
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
+
 include_file('core', 'authentification', 'php');
+
 if (!isConnect()) {
     include_file('desktop', '404', 'php');
     die();
 }
+
 ?>
 <form class="form-horizontal">
     <fieldset>
         <div class="form-group">
             <label class="col-lg-4 control-label">{{Global param 1}}</label>
             <div class="col-lg-2">
-                <input class="configKey form-control" data-l1key="param1" />
+                <input type="text" class="configKey form-control" data-l1key="param1" value="{{Default value}}"/>
             </div>
         </div>
         <div class="form-group">
             <label class="col-lg-4 control-label">{{Global param 2}}</label>
             <div class="col-lg-2">
-                <input class="configKey form-control" data-l1key="param2" value="80" />
+                <input type="checkbox" class="configKey form-control" data-l1key="param2" checked="checked" />
             </div>
         </div>
         <div class="form-group">
-            <label class="col-lg-4 control-label">{{Global param 2}}</label>
+            <label class="col-lg-4 control-label">{{Global param 3}}</label>
             <div class="col-lg-2">
                 <select class="configKey form-control" data-l1key="param3">
-                    <option value="value1">value1</option>
-                    <option value="value2">value2</option>
+                    <option value="value1">{{First value}}</option>
+                    <option value="value2">{{Second value}}</option>
                 </select>
             </div>
         </div>
