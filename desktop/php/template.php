cd24ff5693c253daf5abebef87127ae8dc35af08
Loïc <loic@jeedom.com>
2019-03-19 12:53:29 +0100

    Update template.php

--- desktop/php/template.php
+++ desktop/php/template.php
@@ -30,7 +30,7 @@ foreach ($eqLogics as $eqLogic) {
 	$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
 	echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
 	echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
-	echo "<br>";
+	echo '<br>';
 	echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
 	echo '</div>';
 }
