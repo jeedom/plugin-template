97284f5426af9d76358c8f0a821b2dceeecd610e
Bbillyben <47628473+Bbillyben@users.noreply.github.com>
2020-11-04 21:45:53 +0100

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -16,6 +16,8 @@
  */
 
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
+
+
 include_file('core', 'authentification', 'php');
 if (!isConnect()) {
     include_file('desktop', '404', 'php');
@@ -24,7 +26,40 @@ if (!isConnect()) {
 ?>
 <form class="form-horizontal">
     <fieldset>
-        
+        <div class="form-group">
+			<label class="col-lg-4 control-label">{{Supprimer tous les fichiers Attestations}}</label>
+			<div class="col-lg-5">
+				<a class="btn btn-warning" id="bt_syncconfigBlea"><i class="fas fa-sync-alt"></i> {{Effacer}}</a>
+			</div>
+		</div>
   </fieldset>
 </form>
 
+<script>
+   $('#bt_syncconfigBlea').on('click', function () {
+  	if (!confirm('{{Supprimer tous les fichiers attestations conservés de tous les équipement CovidAttest ?}}')) {
+      	$('#div_alert').showAlert({message: '{{Opération annulée}}', level: 'success'});
+        return false;
+    }
+	 $.ajax({// fonction permettant de faire de l'ajax
+            type: "POST", // methode de transmission des données au fichier php
+            url: "plugins/CovidAttest/core/ajax/CovidAttest.ajax.php", // url du fichier php
+            data: {
+                action: "delete_allFiles",
+				level : $(this).attr('data-log')
+            },
+            dataType: 'json',
+            error: function (request, status, error) {
+                handleAjaxError(request, status, error);
+            },
+            success: function (data) { // si l'appel a bien fonctionné
+                if (data.state != 'ok') {
+                    $('#div_alert').showAlert({message: data.result, level: 'danger'});
+                    return;
+                }
+                $('#div_alert').showAlert({message: '{{Réussie}}', level: 'success'});
+            }
+        });
+});
+
+</script>
