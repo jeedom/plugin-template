13fca9217d0f2e281df13f6679461627434a5a35
Bben <47628473+Bbillyben@users.noreply.github.com>
2020-11-11 16:00:48 +0100

    Update install_apt.sh

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -8,9 +8,17 @@ echo 0 > ${PROGRESS_FILE}
 echo "Launch install of CovidAttest dependancy"
 apt-get update
 echo 50 > ${PROGRESS_FILE}
+echo "Installation ImageMagick"
+
+apt-get install -y imagemagick
+
+echo 90 > ${PROGRESS_FILE}
+
 echo "modify conf file"
 
 path=$(find /etc/ImageMagick* -name policy.xml -execdir pwd  \;)
+
+echo "path to conf file : $path"
 
 if [ -d "$path" ]
 then
@@ -19,9 +27,10 @@ then
         cat "$path/policy.xml" | sed -e 's#<policy domain="coder" rights="none" pattern="PDF" />#<!-- policy domain="coder" rights="none" pattern="PDF" /-->#gi' > "$path/temp-file"
 
 	mv "$path/temp-file" "$path/policy.xml"
+	echo "Everything is successfully installed!"
 else
         echo "policy.xml not found" > ${PROGRESS_FILE}
 fi
 
 echo 100 > ${PROGRESS_FILE}
-echo "Everything is successfully installed!"
+
