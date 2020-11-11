5c0685aef79b5bba429fac921d2193cc23532ecb
Bben <47628473+Bbillyben@users.noreply.github.com>
2020-11-11 15:43:10 +0100

    Create install_apt.sh

---
+++ resources/install_apt.sh
@@ -0,0 +1,27 @@
+PROGRESS_FILE=/tmp/dependancy_covidattest_in_progress
+if [ ! -z $1 ]; then
+	PROGRESS_FILE=$1
+fi
+touch ${PROGRESS_FILE}
+echo 0 > ${PROGRESS_FILE}
+echo 0 > ${PROGRESS_FILE}
+echo "Launch install of CovidAttest dependancy"
+apt-get update
+echo 50 > ${PROGRESS_FILE}
+echo "modify conf file"
+
+path=$(find /etc/ImageMagick* -name policy.xml -execdir pwd  \;)
+
+if [ -d "$path" ]
+then
+        echo "policy.xml found in $path" > ${PROGRESS_FILE}
+
+        cat "$path/policy.xml" | sed -e 's#<policy domain="coder" rights="none" pattern="PDF" />#<!-- policy domain="coder" rights="none" pattern="PDF" /-->#gi' > "$path/temp-file"
+
+	mv "$path/temp-file" "$path/policy.xml"
+else
+        echo "policy.xml not found" > ${PROGRESS_FILE}
+fi
+
+echo 100 > ${PROGRESS_FILE}
+echo "Everything is successfully installed!"
