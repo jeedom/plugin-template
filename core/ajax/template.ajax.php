d2fd682d0559b7047b72b3adc29958e5297183d9
Salvialf <jeuxalf@live.fr>
2022-01-20 10:23:08 +0100

    optimisations V4.2

--- core/ajax/template.ajax.php
+++ core/ajax/template.ajax.php
@@ -1,5 +1,4 @@
 <?php
-
 /* This file is part of Jeedom.
  *
  * Jeedom is free software: you can redistribute it and/or modify
@@ -23,18 +22,18 @@ try {
     if (!isConnect('admin')) {
         throw new Exception(__('401 - Accès non autorisé', __FILE__));
     }
-    
+
   /* Fonction permettant l'envoi de l'entête 'Content-Type: application/json'
     En V3 : indiquer l'argument 'true' pour contrôler le token d'accès Jeedom
     En V4 : autoriser l'exécution d'une méthode 'action' en GET en indiquant le(s) nom(s) de(s) action(s) dans un tableau en argument
-  */  
+  */
     ajax::init();
 
 
 
-    throw new Exception(__('Aucune méthode correspondante à : ', __FILE__) . init('action'));
+    throw new Exception(__('Aucune méthode correspondante à', __FILE__) . ' : ' . init('action'));
     /*     * *********Catch exeption*************** */
-} catch (Exception $e) {
+}
+catch (Exception $e) {
     ajax::error(displayException($e), $e->getCode());
 }
-
