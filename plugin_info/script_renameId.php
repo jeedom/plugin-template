00e28055383d1a4c5e843fd8616a56bb797d6a8b
Julien C. <julien@jeedom.com>
2024-12-18 15:44:21 +0100

    add choices in script

--- plugin_info/script_renameId.php
+++ plugin_info/script_renameId.php
@@ -53,26 +53,68 @@ function processDirectories($directories, $newId) {
     }
 }
 
-echo "Entrez le nouvel ID du plugin : ";
+$pathInfoJson = __DIR__ . '/info.json';
+$jsonContent = file_get_contents($pathInfoJson);
+$dataJson  = json_decode($jsonContent, true);
+
+
+echo "Quel est le nom de votre plugin ? ";
+$namePlugin = trim(fgets(STDIN));
+
+
+if ($namePlugin !== '') {
+    $dataJson['name'] = $namePlugin;
+    echo "Plugin renommé.\n";
+} 
+
+echo "Votre plugin possede t-il un démon ? (oui/non) : ";
+$demonResponse = trim(fgets(STDIN));
+
+if (strtolower($demonResponse) === 'oui' || strtolower($demonResponse) === 'o') {
+
+    $dataJson['hasOwnDeamon'] = true; 
+    echo "La prise en compte du démon est activée.\n";
+} 
+
+
+echo "Votre plugin possede t-il des dépendances ? (oui/non) : ";
+$dependancyResponse = trim(fgets(STDIN));
+
+
+
+if (strtolower($dependancyResponse) === 'oui' || strtolower($dependancyResponse) === 'o') {
+
+    $dataJson['hasDependency'] = true; 
+    echo "La prise en compte des dépendances est activée.\n";
+} 
+
+
+echo "Quel est l'ID du plugin : ";
 $newId = trim(fgets(STDIN));
 
 echo "Vous avez entré '$newId'. Confirmez-vous cette modification ? (oui/non) : ";
 $confirmation = trim(fgets(STDIN));
 
-$directories = [
-    __DIR__ . '/../core/class', 
-    __DIR__ . '/../desktop',
-    __DIR__ . '/../core/php',
-    __DIR__ . '/../desktop/modal',
-    __DIR__ . '/../desktop/php',
-    __DIR__ . '/../desktop/js',
-    __DIR__ ,
-];
-
 
 if (strtolower($confirmation) === 'oui' || strtolower($confirmation) === 'o') {
+
+    $dataJson['id'] = $newId; 
+
+    $directories = [
+        __DIR__ . '/../core/class', 
+        __DIR__ . '/../desktop',
+        __DIR__ . '/../core/php',
+        __DIR__ . '/../desktop/modal',
+        __DIR__ . '/../desktop/php',
+        __DIR__ . '/../desktop/js',
+        __DIR__ ,
+    ];
+     
     processDirectories($directories, $newId);
     echo "L'ID du plugin a été remplacé et les fichiers ont été renommés avec succès.\n";
 } else {
     echo "Modification annulée.\n";
 }
+
+$newJsonContent = json_encode($dataJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+file_put_contents($pathInfoJson, $newJsonContent);
